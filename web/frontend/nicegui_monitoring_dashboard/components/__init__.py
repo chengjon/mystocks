9f3e73e5ef6cab91c38e3461fc2353d9276382b5
iFlow User <user@example.com>
2025-12-06 09:53:37 +0800

    chore: 移除所有NiceGUI相关代码和文档

--- web/frontend/nicegui_monitoring_dashboard/components/__init__.py
+++
@@ -1,8 +0,0 @@
-# 组件索引文件
-
-from .kline_charts import *
-from .realtime_charts import *
-from .alert_panel import *
-from .control_panel import *
-from .floating_actions import *
-from .utility import *
