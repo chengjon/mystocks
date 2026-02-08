0aebc4d77a3bca436cd875dafcb5109d0f53e9f1
JohnC <ninjas@sina.com>
2026-02-08 08:42:27 +0800

    refactor(frontend): Split ArtDecoDataAnalysis.vue into modular components

---
+++ web/frontend/src/views/artdeco-pages/components/AnalysisResults.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="results-container">
+        <ArtDecoCard title="选股结果" hoverable>
+            <ArtDecoTable
+                :columns="columns"
+                :data="data"
+                @row-click="$emit('row-click', $event)"
+            />
+        </ArtDecoCard>
+    </div>
+</template>
+
+<script setup>
+import { ArtDecoCard, ArtDecoTable } from '@/components/artdeco'
+
+defineProps({
+    columns: Array,
+    data: Array
+})
+
+defineEmits(['row-click'])
+</script>
