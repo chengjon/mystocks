189f73668ea5cd19af665f1ae5c18b98d5c34d3e
JohnC <ninjas@sina.com>
2026-04-19 10:49:41 +0800

    fix(core): re-export data manager

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -2,12 +2,14 @@
 from .config_driven_table_manager import ConfigDrivenTableManager  # noqa: F401
 from .data_classification import DataClassification  # noqa: F401
 from .data_classification import DatabaseTarget  # noqa: F401
+from .data_manager import DataManager  # noqa: F401
 from .deduplication_strategy import DeduplicationStrategy  # noqa: F401
 from .unified_manager import MyStocksUnifiedManager  # noqa: F401
 
 __all__ = [
     "ConfigDrivenTableManager",
     "DataClassification",
+    "DataManager",
     "DatabaseTarget",
     "DeduplicationStrategy",
     "MyStocksUnifiedManager",
